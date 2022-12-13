7ff5f709a7c25c94bb3b85b4ebdd30695ec8dc07
Jorge D. Ortiz Fuentes <jorge.ortiz-fuentes@mongodb.com>
2022-12-13 12:35:39 +0100

    Second test for state

--- src/supervillain.rs
+++ src/supervillain.rs
@@ -7,6 +7,11 @@ impl Supervillain {
     pub fn full_name(&self) -> String {
         format!("{} {}", self.first_name, self.last_name)
     }
+    pub fn set_full_name(&mut self, name: &str) {
+        let components = name.split(" ").collect::<Vec<_>>();
+        self.first_name = components[0].to_string();
+        self.last_name = components[1].to_string();
+    }
 }
 
 #[cfg(test)]
@@ -24,5 +29,18 @@ mod tests {
         let full_name = sut.full_name();
         // Assert
         assert_eq!(full_name, "Lex Luthor", "Unexpected full name");
+    }
+    #[test]
+    fn set_full_name_sets_first_and_last_name() {
+        // Arrange
+        let mut sut = Supervillain {
+            first_name: "Lex".to_string(),
+            last_name: "Luthor".to_string(),
+        };
+        // Act
+        sut.set_full_name("Darth Vader");
+        // Assert
+        assert_eq!(sut.first_name, "Darth");
+        assert_eq!(sut.last_name, "Vader");
     }
 }
